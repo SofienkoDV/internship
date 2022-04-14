8c4fd6971767523dbba6f76dc80cead6eab14132
nero <sofienko2933@gmail.com>
2022-04-14 22:33:28 +0300

    Добавил бургер

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,20 @@
+import { useCallback, useState } from 'react';
+import cn from 'classnames';
+import cnBind from "classnames/bind";
 import styles from './Header.module.scss';
 import logo from '../../assets/logo.png';
 
 function Header() {
+    const cx = cnBind.bind(styles);
+    const [burger, setBurger] = useState(false);
+    const burgerMenu = cn(burger ? cx('navShow') : cx('nav'));
+
+
+    const onClickBurger = useCallback(() => {
+        setBurger((burger) => !burger);
+      }, [burger]);
+
+
     return (
     <header className={styles.header}>
       <div className={styles.container}>
@@ -11,7 +24,7 @@ function Header() {
                   <h1>ReactApp</h1>
               </div>
   
-              <nav className={styles.nav}>
+              <nav className={burgerMenu}>
                   <a className={styles.nav__link} href="#">Home</a>
                   <a className={styles.nav__link} href="#">About</a>
                   <a className={styles.nav__link} href="#">Gallary</a>
@@ -19,7 +32,7 @@ function Header() {
                   <a className={styles.nav__link} href="#">Contact</a>
               </nav>
   
-              <button className={styles.burger}>
+              <button className={styles.burger} onClick={onClickBurger}>
                   <span className={styles.burger__item}>Menu</span>
               </button>
   
