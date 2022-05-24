25420d9aca4a9e8a520c53586e319066729562f0
SofienkoDV <sofienko2933@gmail.com>
2022-05-24 22:45:16 +0300

    24.05

--- src/components/FeaturesItem/FeaturesItem.jsx
+++ src/components/FeaturesItem/FeaturesItem.jsx
@@ -1,16 +1,18 @@
-import styles from './FeaturesItem.module.scss';
-
+import styles from './FeaturesItem.module.scss'
+import FeatureOne from '../../assets/features/1.png'
 
 function FeaturesItem(props) {
-  return (
-        <div className={styles.features__item}>
-            <img className={styles.features__icon} src={props.imageUrl} alt={props.alt} />
-            <h4 className={styles.features__title}>{props.title}</h4>
-            <div className={styles.features__text}>
-                The standard chunk of Lorem Ipsum used since the is reproduced below for those interested
-            </div>
-        </div>
-  )
+	console.log(props.imageUrl)
+	return (
+		<div className={styles.features__item}>
+			<img className={styles.features__icon} src={FeatureOne} alt={props.alt} />
+			<h4 className={styles.features__title}>{props.title}</h4>
+			<div className={styles.features__text}>
+				The standard chunk of Lorem Ipsum used since the is reproduced below for
+				those interested
+			</div>
+		</div>
+	)
 }
 
-export default FeaturesItem;
+export default FeaturesItem
--- src/pages/Biography/Biography.jsx
+++ src/pages/Biography/Biography.jsx
@@ -1,43 +1,39 @@
-import { useContext, useState } from 'react';
-import BiographyItem from '../../components/BiographyItem/BiographyItem';
-import AppContext from '../../context';
-import styles from './Biography.module.scss';
+import { useContext, useState } from 'react'
+import BiographyItem from '../../components/BiographyItem/BiographyItem'
+import AppContext from '../../context'
+import styles from './Biography.module.scss'
 
 function Biography() {
-    const { items } = useContext(AppContext);
-    const [value , setValue] = useState('');
+	const { items } = useContext(AppContext)
+	const [value, setValue] = useState('')
 
-    const filterName = items.filter(item => {
-        return item.name.toLowerCase().includes(value.toLowerCase())
-    });
+	const filterName = items.filter(item => {
+		return item.name.toLowerCase().includes(value.toLowerCase())
+	})
 
-return (
-    <div className={styles.container}>
-        <div className={styles.BiographyForm}>
-            <input 
-                className={styles.BiographyInput}
-                type="text"
-                placeholder="Шукати за ім'ям..."
-                onChange={(e) => setValue(e.target.value)}
-            />
-        </div>
-        
-        <div className={styles.Biography}>
-            {filterName.map((item) => (
-              <BiographyItem
-                key={item.id}
-                name={item.name}
-                imageUrl={item.imageUrl}
-                cash={item.cash}
-                cryptocurrency={item.cryptocurrency.map(({amount}) => amount)}
-              />
-            ))}
-        </div>
-    </div>
-    );
-  }
-  
-export default Biography;
+	return (
+		<div className={styles.container}>
+			<div className={styles.BiographyForm}>
+				<input
+					className={styles.BiographyInput}
+					type='text'
+					placeholder="Шукати за ім'ям..."
+					onChange={e => setValue(e.target.value)}
+				/>
+			</div>
 
+			<div className={styles.Biography}>
+				{filterName.map(item => (
+					<BiographyItem
+						key={item.id}
+						name={item.name}
+						cash={item.cash}
+						cryptocurrency={item.cryptocurrency.map(({ amount }) => amount)}
+					/>
+				))}
+			</div>
+		</div>
+	)
+}
 
-
+export default Biography
